353daf50a22183a33a5f78b5a90a963f908b9568
Gaurav Solankar <gaurav.solankar7@gmail.com>
2025-06-25 18:40:16 +0530

    In movie tab add a hard coded movie

---
+++ QuickMovie/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickMovie.Models;
+
+namespace QuickMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Inception" };
+            return View(movie);
+        }
+    }
+}
