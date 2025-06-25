d375cddab1b53ef1da195457d83844934dbf3950
Gaurav Solankar <gaurav.solankar7@gmail.com>
2025-06-25 18:05:26 +0530

    Added method in the home controller

--- QuickMovie/Controllers/HomeController.cs
+++ QuickMovie/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using QuickMovie.Models;
 
 namespace QuickMovie.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
-        { 
 
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
         }
     }
 }
